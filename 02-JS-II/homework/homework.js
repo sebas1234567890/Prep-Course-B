9c38daea49ffe02cdbbccbd79a351d6a999a4b46
seba1234567890 <soto04196@gmail.com>
2023-02-03 11:23:31 -0300

    seba soto

--- 02-JS-II/homework/homework.js
+++ 02-JS-II/homework/homework.js
@@ -6,34 +6,41 @@
 
 function devolverString(str) {
     // "Return" la string provista: str
-    // Tu código:
+    // Tu código: 
+    return str
   }
   
   function suma(x, y) {
     // "x" e "y" son números
     // Suma "x" e "y" juntos y devuelve el valor
     // Tu código:
-  }
+    return x + y 
+
+  };
   
   function resta(x, y) {
     // Resta "y" de "x" y devuelve el valor
     // Tu código:
+    return x - y 
   }
   
   function multiplica(x, y) {
     // Multiplica "x" por "y" y devuelve el valor
     // Tu código:
+    return x * y 
   }
   
   function divide(x, y) {
     // Divide "x" entre "y" y devuelve el valor
     // Tu código:
+    return x / y 
   }
   
   function sonIguales(x, y) {
     // Devuelve "true" si "x" e "y" son iguales
     // De lo contrario, devuelve "false"
     // Tu código:
+    return x == y
   }
   
   function tienenMismaLongitud(str1, str2) {
